bbb0621ae67e41845cfaf69a8b0200068d9d6fed
TonjeFM <tonjfm15@uia.no>
2022-01-27 18:00:26 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Kaduhuy/rc1-verden
+
+go 1.17
+
+require github.com/digitnow/rivercrossing v0.3.0
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/tonjfm/rivercrossing/event"
+	"os"
+
+	//"rc1-verden/event"
+)
+
+/*var items = [] string{
+
+"kylling",
+"rev",
+"korn",
+}*/
+
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Println("Hva vil du sende over? --> kylling, rev eller korn")
+		scanner.Scan()
+		text := scanner.Text()
+		fmt.Println(event.FirstPut(text))
+		fmt.Println(event.GetIn(text))
+		fmt.Println(event.CrossRiver(text))
+		fmt.Println(event.TakeOut(text))
+		fmt.Println(event.GetOut(text))
+		if(text == "kylling"){
+			break
+		}
+	}
+
+}
